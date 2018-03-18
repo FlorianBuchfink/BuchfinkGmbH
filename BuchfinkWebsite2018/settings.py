62be156fd905f945ef0f896950b6d47b277248f9
FlorianBuchfink <florian.buchfink@googlemail.com>
2018-03-18 20:49:31 +0100

    Update settings.py

--- BuchfinkWebsite2018/settings.py
+++ BuchfinkWebsite2018/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'florian.buchfink@googlemail.com'
-EMAIL_HOST_PASSWORD = 'google$$mail1234'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
