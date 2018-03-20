6e05410f7af6ee2f8e311150c9985c7c0c968ba1
Buchfink <florian.buchfink@googlemail.com>
2018-03-20 22:07:22 +0100

    h

--- BuchfinkWebsite2018/settings.py
+++ BuchfinkWebsite2018/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = False
 ALLOWED_HOSTS = [buchfinkwebsite.herokuapp.com]
 
 
